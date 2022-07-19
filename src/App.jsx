e672daf1b87bec126dee67e770a19075658f20db
Adrian <aracosta2810@gmail.com>
2022-07-19 14:25:08 -0400

    Security saved

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
+import ContactUsForm from "./Components/ContactUsForm";
 import Footer from "./Components/Footer";
 import Hero from "./Components/Hero";
 import NavBar from "./Components/NavBar";
+import TextInformation from "./Components/TextInformation";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Hero />
+      <ContactUsForm/>
+      <TextInformation/>
+
       <Footer/>
     </div>
   );
